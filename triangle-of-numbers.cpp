c30516e6a3cd55911b156f6a65d85f1f06fc97c7
sac6inarya <sachinkarya333@gmail.com>
2023-09-13 21:39:41 +0530

    inverted half pyramid star

--- triangle-of-numbers.cpp
+++ triangle-of-numbers.cpp
@@ -5,9 +5,17 @@ void sol()
 {
     for (int i = 1; i <= 5; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= 5; j++)
         {
-            cout << "* ";
+            if (j >= 6 - i)
+            {
+
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
         }
         cout << endl;
     }
